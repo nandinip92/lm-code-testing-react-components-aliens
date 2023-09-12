cd0bf8695178fbb430ab30f89b461ee34f548739
nandinip92 <nandini.p8892@gmail.com>
2023-09-12 08:56:31 +0100

    tests: Adding new tests to check the validate function call and error returns

--- src/components/planet-name.test.tsx
+++ src/components/planet-name.test.tsx
@@ -56,4 +56,57 @@ describe("<PlanetName/>", () => {
     //to ensure that a mock function got called exact number of times. Also under the alias: .toBeCalledTimes(number)
     expect(mockOnChange).toHaveBeenCalledTimes(1);
   });
+
+  //props for checking validate functions and below are the 3 tests
+  const mockValidatePlanetName = jest.fn();
+  const requiredProps: PlanetNameProps = {
+    planetName: "",
+    onChangePlanetName: () => {},
+    validate: mockValidatePlanetName,
+  };
+  it(`Given the required props,
+  when the text is typed in the text box, 
+  input field should call its 'validate' function to return empty [] when Species name is Valid`, () => {
+    //Act
+    mockValidatePlanetName.mockReturnValue([]);
+
+    render(<PlanetName {...requiredProps} />);
+    //Checking for the non-existance of the element <p>
+    const noErrorOnScreen = screen.queryByTestId(/error/i);
+    expect(noErrorOnScreen).toBeNull();
+  });
+  it(`Given the required props,
+  when the text is typed in the text box,
+  input field should call its 'validate' function to return ONE Error`, () => {
+    //Arrange
+    const errorMessage = "Characters length must be between 2 and 49";
+
+    //Act
+    mockValidatePlanetName.mockReturnValue([errorMessage]); // return one error
+    render(<PlanetName {...requiredProps} />);
+
+    const isOneError = screen.queryAllByTestId(/error/i);
+    const oneErrorOnScreen = screen.getByText(errorMessage);
+
+    //Assert
+    expect(isOneError).toHaveLength(1); //check if there is only one error
+    expect(oneErrorOnScreen).toBeInTheDocument();
+  });
+  it(`Given the required props,
+  when the text is typed in the text box, 
+  input field should call its 'validate' function to return TWO Errors`, () => {
+    //Arrange
+    const errorMessage = [
+      "Characters length must be between 2 and 49",
+      "Numbers are allowed, but no special characters!",
+    ];
+    //Act
+    mockValidatePlanetName.mockReturnValue(errorMessage); // // return two errors
+    render(<PlanetName {...requiredProps} />);
+
+    const isTwoErrors = screen.queryAllByTestId(/error/i);
+    const twoErrorOnScreen = screen.getByText(errorMessage[1]);
+    expect(isTwoErrors).toHaveLength(2); //check if there is only one error
+    expect(twoErrorOnScreen).toBeInTheDocument();
+  });
 });
