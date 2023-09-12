8fa256963e352efa8cde6975e3c731b504253dcf
nandinip92 <nandini.p8892@gmail.com>
2023-09-12 14:34:46 +0100

    tests: Adding new tests to check the validate function call and error returns

--- src/components/number-of-beings.test.tsx
+++ src/components/number-of-beings.test.tsx
@@ -53,4 +53,57 @@ describe("<NumberOfBeings/>", () => {
     expect(mockOnChange).toHaveBeenCalledTimes(1);
     expect(mockOnChange).toHaveBeenCalledWith("201");
   });
+
+  //props for checking validate functions and below are the 3 tests
+  const mockValidateNumberOfBeingsProps = jest.fn();
+  const requiredProps: NumberOfBeingsProps = {
+    numOfBeings: "20",
+    onChangeNumOfBeings: () => {},
+    validate: mockValidateNumberOfBeingsProps,
+  };
+  it(`Given the required props,
+  when the text is typed in the text box, 
+  input field should call its 'validate' function to return empty [] when Species name is Valid`, () => {
+    //Act
+    mockValidateNumberOfBeingsProps.mockReturnValue([]);
+
+    render(<NumberOfBeings {...requiredProps} />);
+    //Checking for the non-existance of the element <p>
+    const noErrorOnScreen = screen.queryByTestId(/error/i);
+    expect(noErrorOnScreen).toBeNull();
+  });
+  it(`Given the required props,
+  when the text is typed in the text box,
+  input field should call its 'validate' function to return ONE Error`, () => {
+    //Arrange
+    const errorMessage = "ONLY Numbers!";
+
+    //Act
+    mockValidateNumberOfBeingsProps.mockReturnValue([errorMessage]); // return one error
+    render(<NumberOfBeings {...requiredProps} />);
+
+    const isOneError = screen.queryAllByTestId(/error/i);
+    const oneErrorOnScreen = screen.getByText(errorMessage);
+
+    //Assert
+    expect(isOneError).toHaveLength(1); //check if there is only one error
+    expect(oneErrorOnScreen).toBeInTheDocument();
+  });
+  it(`Given the required props,
+  when the text is typed in the text box, 
+  input field should call its 'validate' function to return TWO Errors`, () => {
+    //Arrange
+    const errorMessage = [
+      "ONLY Numbers!",
+      "numberOfBeings must be atleast 1,000,000,000",
+    ];
+    //Act
+    mockValidateNumberOfBeingsProps.mockReturnValue(errorMessage); // // return two errors
+    render(<NumberOfBeings {...requiredProps} />);
+
+    const isTwoErrors = screen.queryAllByTestId(/error/i);
+    const twoErrorOnScreen = screen.getByText(errorMessage[1]);
+    expect(isTwoErrors).toHaveLength(2); //check if there is only one error
+    expect(twoErrorOnScreen).toBeInTheDocument();
+  });
 });
